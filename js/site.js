d9cbe8b21093eb3c9cd51a66892b3a9a3b418ec0
tonyh0318 <96548981+tonyh0318@users.noreply.github.com>
2022-01-25 07:44:33 -0800

    1

--- js/site.js
+++ js/site.js
@@ -1,13 +1,77 @@
-//display a message to the user
-function getMessage() {
+const events = [{
+        id: 1,
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 240000,
+        date: "06/01/2017",
+    },
+    {
+        id: 2,
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 250000,
+        date: "06/01/2018",
+    },
+    {
+        id: 3,
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 257000,
+        date: "06/01/2019",
+    },
+    {
+        id: 4,
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 130000,
+        date: "06/01/2017",
+    },
+    {
+        id: 5,
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 140000,
+        date: "06/01/2018",
+    },
+    {
+        id: 6,
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 150000,
+        date: "06/01/2019",
+    },
+    {
+        id: 7,
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 40000,
+        date: "06/01/2017",
+    },
+    {
+        id: 8,
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 45000,
+        date: "06/01/2018",
+    },
+    {
+        id: 9,
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 50000,
+        date: "06/01/2019",
+    },
+];
 
-    let userMessage = document.getElementById("message").value;
-    
-    Swal.fire(
-        'The Internet?',
-        'That thing is still around?',
-        'question'
-    )
-    
-    console.log(userMessage);
+function buildDropDown() {
+    let eventDD = document.getElementById("");
 }
